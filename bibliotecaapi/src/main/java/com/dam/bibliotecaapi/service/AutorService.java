34be677b74139feb76ed2e4c57aecb89f11c56f5
JorgeVeres <moganero292@gmail.com>
2025-05-18 15:59:09 +0100

    Serivice del autor añadido

---
+++ bibliotecaapi/src/main/java/com/dam/bibliotecaapi/service/AutorService.java
@@ -0,0 +1,26 @@
+package main.java.com.dam.bibliotecaapi.service;
+
+import com.tuapp.bibliotecaapi.model.Autor;
+import com.tuapp.bibliotecaapi.repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AutorService {
+    @Autowired
+    private AutorRepository autorRepository;
+
+    public List<Autor> findAll() {
+        return autorRepository.findAll();
+    }
+
+    public Optional<Autor> findById(Long id) {
+        return autorRepository.findById(id);
+    }
+
+    public Autor save(Autor autor) {
+        return autorRepository.save(autor);
+    }
+}
